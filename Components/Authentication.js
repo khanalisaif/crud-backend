9ce7c4288186ffd533c873fd0e31e3d75aa0d8d5
Firoz Akhter <firozakhter789@gmail.comgit config --list>
2025-05-08 14:02:01 +0530

    almost done, might require minor update/code

--- Components/Authentication.js
+++ Components/Authentication.js
@@ -1,33 +1,34 @@
-import jwt from 'jsonwebtoken';
-import User from './UserModel.js';
+import jwt from "jsonwebtoken";
+import User from "./UserModel.js";
 
 export const authenticateUser = async (req, res, next) => {
   try {
-    // console.log("req.body..", req.body);
-    const token = req.header('Authorization')?.split(' ')[1]; 
-    console.log("token..", token);
+    console.log("Inside authenticateUser");
+    const token = req.header("Authorization")?.split(" ")[1];
+    // console.log("token..", token);
 
     if (!token) {
-      return res.status(401).json({ error: 'Access denied, no token provided' });
+      return res
+        .status(401)
+        .json({ error: "Access denied, no token provided" });
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); 
-    console.log("decoded..", decoded)
-    req.user = await User.findById(decoded.id).select('-password'); 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("decoded..", decoded);
+    const email = decoded.email;
+    const user = await User.findOne({ email });
 
-    if (!req.user) {
-      return res.status(404).json({ error: 'User not found' });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
 
-    
-    req.body.email = decoded.email;
-    // console.log("req.body...", req.body); 
-    next(); 
+    req.user = user;
+    // console.log("req.user..", req.user);
+    // console.log("somewhere");
+    next();
   } catch (error) {
-    res.status(401).json({ error: 'Invalid or expired token' });
+    res.status(401).json({ error: "Invalid or expired token" });
   }
 };
-
-
 
 export default authenticateUser;
--- Components/TodoModel.js
+++ Components/TodoModel.js
@@ -1,11 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
   },
- 
+  email: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
 });
 
-export default mongoose.model('Todo', TodoSchema);
+export default mongoose.model("Todo", TodoSchema);
